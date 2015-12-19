113ff81504358f4f94040566327055684d0ccd3c
Dan Yawitz <daniel.yawitz@gmail.com>
2015-12-19 17:28:08 -0500

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,5 +1,6 @@
 # Hint:  use Google to find python function
 monthDict={1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+monthDictInv={'Feb': 2, 'Jan': 1, 'Sep': 9, 'Oct': 10, 'Dec': 12, 'Jun': 6, 'Jul': 7, 'May': 5, 'Mar': 3, 'Nov': 11, 'Aug': 8, 'Apr': 4}
 monthExtraDict={1:1, 2:-2, 3:1, 4:0, 5:1, 6:0, 7:1, 8:1, 9:0, 10:1, 11:0, 12:1} 
 
 def get_extradays(start, stop):
@@ -12,11 +13,14 @@ def get_extradays(start, stop):
   
 def get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop):
   year_dif = year_stop - year start
-  year_days = year_dif * 365
+  year_days = year_dif * 365 
+  
   month_dif = month_stop - month_start
   extra = get_extradays(month_start, month_stop)
   month_days = month_days * 30 + extra
+  
   day_days = abs(day_stop - day_start)
+  
   total = year_days + month_days + day_days
   return total
 
@@ -34,7 +38,7 @@ day_start = int(date_start[0:1])
 day_stop = int(day_stop[0:1])
 
 TIME_A = get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop)
-print(TIME_A)
+print("There are ", TIME_A, " days between", date_start, " and ", date_stop)
 
 ####b)  
 date_start = '12312013'  
@@ -50,20 +54,20 @@ day_start = int(date_start[0:1])
 day_stop = int(day_stop[0:1])
 
 TIME_B = get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop)
-print(TIME_B)
+print("There are ", TIME_B, " days between", date_start, " and ", date_stop)
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
 
-year_start = int(date_start[4:])
-year_stop = int(date_stop[4:])
+year_start = int(date_start[7:])
+year_stop = intdate_stop[7:])
 
-month_start = int(date_start[2:3])
-month_stop = int(date_stop[2:3])
+month_start = int(monthDictInv[date_start[3:6]])
+month_stop = int((monthDictInv[date_stop[3:6]]))
 
 day_start = int(date_start[0:1])
 day_stop = int(day_stop[0:1])
 
 TIME_C = get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop)
-print(TIME_C)
+print("There are ", TIME_C, " days between", date_start, " and ", date_stop)
