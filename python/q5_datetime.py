2eedf4582f28fcd85e8dfae46229f9a51fd1109b
Dan Yawitz <daniel.yawitz@gmail.com>
2015-12-19 17:09:33 -0500

    in progress

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -10,6 +10,15 @@ def get_extradays(start, stop):
     extra = extra + monthExtraDict[key]
   return extra
   
+def get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop):
+  year_dif = year_stop - year start
+  year_days = year_dif * 365
+  month_dif = month_stop - month_start
+  extra = get_extradays(month_start, month_stop)
+  month_days = month_days * 30 + extra
+  day_days = abs(day_stop - day_start)
+  total = year_days + month_days + day_days
+  return total
 
 ####a) 
 date_start = '01-02-2013'  
@@ -17,26 +26,44 @@ date_stop = '07-28-2015'
 
 year_start = int(date_start[6:])
 year_stop = int(date_stop[6:])
-year_dif = year_stop - year start
-year_days = year_dif * 365
 
 month_start = int(date_start[3:4])
 month_stop = int(date_stop[3:4])
-month_dif = month_stop - month_start
-
-extra = get_extradays(month_start, month_stop)
-month_days = month_days * 30 + extra
 
 day_start = int(date_start[0:1])
 day_stop = int(day_stop[0:1])
-day_days = abs(day_stop - day_start)
 
-total = year_days + month_days + day_days
+TIME_A = get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop)
+print(TIME_A)
 
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015'
+
+year_start = int(date_start[4:])
+year_stop = int(date_stop[4:])
+
+month_start = int(date_start[2:3])
+month_stop = int(date_stop[2:3])
+
+day_start = int(date_start[0:1])
+day_stop = int(day_stop[0:1])
+
+TIME_B = get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop)
+print(TIME_B)
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+year_start = int(date_start[4:])
+year_stop = int(date_stop[4:])
+
+month_start = int(date_start[2:3])
+month_stop = int(date_stop[2:3])
+
+day_start = int(date_start[0:1])
+day_stop = int(day_stop[0:1])
+
+TIME_C = get_timediff(year_start, year_stop, month_start, month_stop, day_start, day_stop)
+print(TIME_C)
