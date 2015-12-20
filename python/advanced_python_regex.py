0889228e755fa9bda7248ae68cb1aaa9292bf87a
Dan Yawitz <daniel.yawitz@gmail.com>
2015-12-20 15:16:22 -0500

    Q1

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,50 @@
-PLACE YOUR CODE HERE
+import os, csv, re
+
+
+def read_data(file):
+  data = open(file)
+  d = []
+  csv_data = csv.reader(data)
+  for row in csv_data:
+    d.append(row)
+  data.close()
+  return d
+  
+def get_uniqe_degrees(parsed_data):
+  '''list of lists --> list of tuples
+  
+  Takes the parsed data and returns a list of tuples in the format (str, int)
+  
+  >>> get_unique_degrees(data)
+  [('phd', 3), ('jd', 1)]
+  '''
+  d = parsed_data
+  
+  raw = []
+  for i in range(len(d)):
+    if i > 0:
+      full.append(d[i][1])
+  
+  stripped = []
+  for item in raw:
+    new = re.sub('[.]', '', item.lower())
+    stripped.append(new)
+  
+  degrees = []
+  for item in stripped:
+    if item != '0':
+      personList = re.sub("[^\w]", " ",  item).split()
+      for deg in personList:
+        degrees.append(deg)
+  
+  
+
+
+      
+    
+  
+  
+  
+  
+
+    
