98b73d4f9cd34171e84c419b35670952384f7f88
Dan Yawitz <daniel.yawitz@gmail.com>
2015-12-20 12:26:44 -0500

    done 12/20

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -15,7 +15,11 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
+    count = 0
+    for i in words:
+        if len(i) > 2:
+            count = count + 1
+    return count
 
 
 def front_x(words):
@@ -32,7 +36,14 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    xlist = []
+    reglist = []
+    for i in words:
+        if i[0] == 'x':
+            xlist.append(i)
+        else:
+            reglist.append(i)
+    return sorted(xlist) + sorted(reglist)
 
 
 def sort_last(tuples):
@@ -49,7 +60,8 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+
+    return sorted(tuples, key=lambda t: t[-1])
 
 
 def remove_adjacent(nums):
@@ -68,7 +80,14 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+    l = []
+    for i in range(len(nums)):
+        if i > 0:
+            if nums[i] != nums[i-1]:
+                l.append(nums[i])
+        else:
+            l.append(nums[i])
+    return l
 
 
 def linear_merge(list1, list2):
@@ -85,4 +104,5 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    both = list1 + list2
+    return sorted(both)
