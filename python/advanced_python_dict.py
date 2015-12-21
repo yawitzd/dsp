17e0548a11c0991867a6a2175b7e84a85d9f42c2
Dan Yawitz <daniel.yawitz@gmail.com>
2015-12-20 23:15:40 -0500

    DONE

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,5 +1,5 @@
 import os, csv, re
-
+os.chdir('Metis/prework')
 
 def read_data(file):
   data = open(file)
@@ -18,8 +18,6 @@ def get_names(parsed_data):
       full.append(d[i][0])
   return full
   
-  
-
 def get_info(parsed_data):
   '''Returns a list of the relevant info for each professor'''
   info = []
@@ -28,6 +26,27 @@ def get_info(parsed_data):
       info.append(d[i][1:])
   return info
 
+def faculty_dict(parsed_data):
+  d = parsed_data
+  names = get_names(parsed_data)
+  info = get_info(parsed_data)
+  
+  
+  lasts = []
+  for i in names:
+    l = i.split(' ')
+    lasts.append(l[-1])
+  
+  fac_dict = dict()
+  for i in range(len(lasts)):
+    name = lasts[i]
+    inf = info[i]
+    if name in fac_dict:
+      fac_dict[name].append(inf)
+    else:
+      fac_dict[name] = [inf]
+
+#Q7
 def professor_dict(parsed_data):
   info = get_info(parsed_data)
   names = get_names(parsed_data)
@@ -40,8 +59,23 @@ def professor_dict(parsed_data):
   
   for i in range(len(fl)):
     result[fl[i]] = info[i]
-    
   
+  return result
+
+#Q8
+def professor_dict_rev(parsed_data):
+  info = get_info(parsed_data)
+  names = get_names(parsed_data)
+  lf = []
+  for i in names:
+    l = i.split(' ')
+    lf.append((l[-1], l[0]))
+  
+  result = dict()
+  
+  for i in range(len(lf)):
+    result[lf[i]] = info[i]    
+  return result
   
     
   
